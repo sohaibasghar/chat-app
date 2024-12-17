56b6b74ef8f1773d7ace6e15e05ef9c4ef6f0e72
Sohaib Asghar <sohaib@devigital.com>
2024-12-17 18:15:47 +0500

    versal config added

--- server/src/index.js
+++ server/src/index.js
@@ -16,23 +16,24 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.json({limit: "30mb", extended: true}));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.SOCKET_URL || "*",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
 
-app.use("/test", (req, res) => {
-  res.json({ message: "Welcome to chat app API", status: "success" });
-});
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  console.log("production");
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
